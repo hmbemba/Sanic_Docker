ad08382245188e11d7c4b87f18f206a14f904dd3
hmbemba <hmbemba@gmail.com>
2022-09-06 17:28:42 -0400

    made more simple

--- server.py
+++ server.py
@@ -6,20 +6,20 @@ from jinja2.loaders import FileSystemLoader
 from jinja2 import Environment
 from sanic.response import text
 
-app = Sanic("MyHelloWorldApp")
-app.blueprint([user_bp, medium_bp])
+# app = Sanic("MyHelloWorldApp")
+# app.blueprint([user_bp, medium_bp])
 
-@app.before_server_start
-def setup_template_env(app, _):
-    app.ctx.env = Environment(
-        loader=FileSystemLoader(Path(__file__).parent /"templates"),
-        autoescape=True,
-    )
-# app = Sanic("MySanicApp")
+# @app.before_server_start
+# def setup_template_env(app, _):
+#     app.ctx.env = Environment(
+#         loader=FileSystemLoader(Path(__file__).parent /"templates"),
+#         autoescape=True,
+#     )
+app = Sanic("MySanicApp")
 
-# @app.get('/')
-# async def hello(request):
-#     return text("OK!")
+@app.get('/')
+async def hello(request):
+    return text("OK!")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
